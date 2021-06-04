396e654debaca187928aabe7228c40654d026388
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-06-04 19:58:41 -0300

    hfix: removed libraries that have wrong path

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
-#include "color/rgb.cpp"
-#include "color/hsv.cpp"
-
 #include <iostream>
 
 int main(int argc, char ** argv)
 {
 
-    auto rgb = RGB(129,88,47);
-
-    auto hsv = HSV(rgb);
-
-    std::cout << hsv.hue << "\n" << hsv.saturation << "\n" << hsv.value << std::endl;
-    
     return 0;
 }
